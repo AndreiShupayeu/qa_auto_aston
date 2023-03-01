d6623dae6f1b29788241b83101172e89fd782427
AndreiShupayeu <andreishupaevqa@gmail.com>
2023-03-01 22:59:04 +0300

    add homework4

--- src/main/java/home_work_5/Task1.java
+++ src/main/java/home_work_5/Task1.java
@@ -1,7 +1,21 @@
 package home_work_5;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+
 //1. написать метод с try-with-resources
 public class Task1 {
     public static void main(String[] args) {
-
+        readFromFile();
     }
+
+    public static void readFromFile() {
+        try (Reader reader = new FileReader(new File("Some path"))) {
+        } catch (IOException e) {
+            System.out.println("We use try-with-resources");
+        }
+    }
+
 }
--- src/main/java/home_work_5/Task4.java
+++ src/main/java/home_work_5/Task4.java
@@ -1,7 +1,19 @@
 package home_work_5;
+
 //4. Создать второй конструктор в котором будет выводится Имя класса и названия того кто его делал
+//Сделай конструктор, который будет принимать класс в качестве параметра. В нем вызови дефолтный
+// конструктор и потом выведи в консоль название класса, переданного в конструктор
 public class Task4 {
     public static void main(String[] args) {
+        ConstructorWithParam constructorWithParam = new ConstructorWithParam(new Param());
+    }
+}
 
+class Param {
+}
+
+class ConstructorWithParam {
+    ConstructorWithParam(Param param) {
+        System.out.println(param.getClass());
     }
 }

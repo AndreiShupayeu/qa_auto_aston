66e3d33a8311e9f34d347d3bb68edebeeace33f1
AndreiShupayeu <andreishupaevqa@gmail.com>
2023-03-02 22:01:04 +0300

    add homework5

--- src/main/java/home_work_5/Task2.java
+++ src/main/java/home_work_5/Task2.java
@@ -1,7 +1,25 @@
 package home_work_5;
+
+import java.util.HashMap;
+import java.util.Map;
+
 //2. * посчитать для стринги сколько раз используется каждый символ (попробовать решить через map)
 public class Task2 {
     public static void main(String[] args) {
+        String sentence = "посчитать для стринги сколько раз используется каждый символ";
+        informSymbolsQuantity(sentence);
+    }
 
+    public static void informSymbolsQuantity(String s) {
+        char[] chars = s.toCharArray();
+        HashMap<Character, Integer> hashMap = new HashMap<>();
+        for (char aChar : chars) {
+            if (hashMap.get(aChar) == null) {
+                hashMap.put(aChar, 1);
+            } else {
+                hashMap.put(aChar, hashMap.get(aChar) + 1);
+            }
+        }
+        System.out.println(hashMap);
     }
 }
